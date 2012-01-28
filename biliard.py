bf3111cc565b8ea74f0f8105dbc20e4ef24dc61a
Filip Pacanowski <fpacanowski@gmail.com>
2012-01-28 17:19:43 +0100

    friction, stability check

--- biliard.py
+++ biliard.py
@@ -6,11 +6,13 @@ import sys
 import threading
 
 RADIUS=20
+POCKET_RADIUS=30
 OFFSET_X=50
 OFFSET_Y=50
 WIDTH=800
 HEIGHT=400
 FPS=60
+FRICTION=0.001/FPS
 black    = (   0,   0,   0)
 white    = ( 255, 255, 255)
 green    = ( 100, 255, 100)
@@ -28,22 +30,35 @@ class Ball:
 		self.pos = [x, y]
 		self.x_velocity = vx
 		self.y_velocity = vy
+	def apply_friction(self, v):
+		if abs(v) < FRICTION:
+			v = 0.0
+		else:
+			v = (v/abs(v))*(abs(v)-FRICTION)
+		return v
 	def next_iter(self):
 		#print self.pos
 		(a,b) = self.pos
 		(a,b) = (a+self.x_velocity, b+self.y_velocity)
 		self.pos = [a,b]
+		self.x_velocity = self.apply_friction(self.x_velocity)
+		self.y_velocity = self.apply_friction(self.y_velocity)
 
 class Simulation:
 	def __init__(self):
 		self.balls = {}
 		self.processed = set()
+		self.pockets = [[0.0, 0.0], [1.0, 0.0], [2.0, 0.0], \
+		                [0.0, 1.0], [1.0, 1.0], [2.0, 1.0]]
 	def add_ball(self, label, x, y, vx, vy):
 		self.balls.update({label: Ball(x,y,vx/FPS,vy/FPS)})
 	def next_iter(self):
 		self.detect_collisions()
 		for b in self.balls.itervalues():
 			b.next_iter()
+		res = self.result
+		self.result = []
+		return res
 	def count_new_velocity(self, vel1, vel2, pos1, pos2):
 		##print pos1,pos2,vel1,vel2
 		v = [vel2[0]-vel1[0], vel2[1]-vel1[1]]
@@ -89,17 +104,34 @@ class Simulation:
 				(ball.x_velocity,ball.y_velocity,ball2.x_velocity, ball2.y_velocity) = \
 					self.count_new_velocity(v1,v2, ball.pos, ball2.pos)
 			self.processed.add(l)
-			
+	def detect_collisions_with_pockets(self, label):
+		ball = self.balls[label]
+		r = float(RADIUS)/HEIGHT + float(POCKET_RADIUS)/HEIGHT
+		for pocket_nr in range(6):
+			pocket = self.pockets[pocket_nr]
+			if distance(ball.pos, pocket) < r**2:
+				self.result.append([label,pocket_nr])
 	def detect_collisions(self):
+		self.result = []
 		for ball in self.balls.iterkeys():
 			self.detect_collisions_with_walls(ball)
 			self.detect_collisions_with_balls(ball)
+			self.detect_collisions_with_pockets(ball)
 		self.processed = set()
+		for label, k in self.result:
+			del self.balls[label]
 	def get_balls(self):
 		return [b.pos for b in self.balls]
 	def step(self, nr):
-		[self.next_iter() for i in range(nr*FPS)]
-
+		res = []
+		for i in range(nr*FPS):
+			res += self.next_iter()
+		return res
+	def is_stable(self):
+		V = 0.0
+		for b in self.balls.values():
+			V += (b.x_velocity+b.y_velocity)
+		return V == 0.0
 class Interface(threading.Thread):
 	def scale_position(self, pos):
 		return (int(0.5*pos[0]*float(WIDTH))+OFFSET_X, int(pos[1]*float(HEIGHT))+OFFSET_Y)
@@ -124,19 +156,12 @@ class Interface(threading.Thread):
 				if event.type == pygame.QUIT: # If user clicked close
 					done=True # Flag that we are done so we exit this loop 
 			screen.fill(green)
-			#balls = sim.balls
-			#a = balls[0]
-			#b = balls[1]
-			#pygame.draw.circle(screen,red,self.scale_position(a.pos),RADIUS)
-			#pygame.draw.circle(screen,black,self.scale_position(b.pos),RADIUS) 
 			pygame.draw.rect(screen,black,[OFFSET_X,OFFSET_Y,WIDTH,HEIGHT], 2)
-			#sim.next_iter()
 			for label in self.balls.iterkeys():
 				pos = self.balls[label]
 				col = self.ball_colors[label]
 				pygame.draw.circle(screen,col,self.scale_position(pos),RADIUS)
 			clock.tick(FPS)
-			#pygame.display.update((ap[0]-20, ap[1]-20,ap[0]+20,ap[0]+20))
 			pygame.display.update()
 		pygame.quit ()
 
@@ -145,10 +170,11 @@ class Biliard:
 		self.gui = Interface()
 		self.gui.start()
 		self.sim = Simulation()
-		self.sim.add_ball(0, 0.2,0.2,0.7,0.7)
-		self.sim.add_ball(1, 0.7,0.7,-0.4,-0.4)
+		self.sim.add_ball(0, 0.2,0.2,-0.3,0.3)
+		self.sim.add_ball(1, 0.7,0.7,-0.6,-0.6)
 		clock = pygame.time.Clock()
 		while True:
+			self.gui.balls = {}
 			for label, ball in self.sim.balls.iteritems():
 				self.gui.balls[label] = ball.pos
 			self.sim.next_iter()
--- test.py
+++ test.py
@@ -13,25 +13,25 @@ class TestSimulation(unittest.TestCase):
 	def test_ball_motion(self):
 		(x, y) = (0.5, 0.5)
 		self.sim.add_ball("ball", x, y, 0.01, 0.01)
-		self.sim.step(10)
+		self.sim.step(2)
 		self.assertNotEqual(x, self.sim.balls["ball"].pos[0])
 	def test_collison_with_wall(self):
-		self.sim.add_ball("ball", 0.5, 0.8, 0.0, 0.05)
-		self.sim.step(10)
+		self.sim.add_ball("ball", 0.5, 0.8, 0.0, 0.3)
+		self.sim.step(3)
 		self.assertTrue(self.sim.balls["ball"].y_velocity < 0)
-		self.sim.add_ball("ball", 0.5, 0.2, 0.0, -0.05)
-		self.sim.step(10)
+		self.sim.add_ball("ball", 0.5, 0.2, 0.0, -0.3)
+		self.sim.step(3)
 		self.assertTrue(self.sim.balls["ball"].y_velocity > 0)
-		self.sim.add_ball("ball", 1.8, 0.5, 0.05, 0.0)
-		self.sim.step(10)
+		self.sim.add_ball("ball", 1.8, 0.5, 0.3, 0.0)
+		self.sim.step(3)
 		self.assertTrue(self.sim.balls["ball"].x_velocity < 0)
-		self.sim.add_ball("ball", 0.2, 0.5, -0.05, 0.0)
-		self.sim.step(10)
+		self.sim.add_ball("ball", 0.2, 0.5, -0.3, 0.0)
+		self.sim.step(3)
 		self.assertTrue(self.sim.balls["ball"].x_velocity > 0)
 	def test_collision_with_two_balls(self):
-		self.sim.add_ball("ball_1", 0.5, 0.5, 0.1, 0.0)
-		self.sim.add_ball("ball_2", 0.9, 0.5, -0.1, 0.0)
-		self.sim.step(2)
+		self.sim.add_ball("ball_1", 0.7, 0.5, 0.3, 0.0)
+		self.sim.add_ball("ball_2", 0.9, 0.5, -0.3, 0.0)
+		self.sim.step(3)
 		self.assertTrue(self.sim.balls["ball_1"].x_velocity < 0)
 		self.assertTrue(self.sim.balls["ball_2"].x_velocity > 0)
 	def test_ball_escaping_bounds(self):
@@ -45,6 +45,19 @@ class TestSimulation(unittest.TestCase):
 		self.sim.step(20)
 		self.assertEqual(self.sim.balls["ball"].x_velocity, 0.0)
 		self.assertEqual(self.sim.balls["ball"].y_velocity, 0.0)
+	def test_ball_disappearing_after_hitting_pocket(self):
+		self.sim.add_ball("ball", 0.5, 0.5, -0.3, -0.3)
+		self.sim.step(3)
+		self.assertEqual(len(self.sim.balls), 0)
+	def test_result_feedback(self):
+		self.sim.add_ball("ball", 0.5, 0.5, -0.3, -0.3)
+		result = self.sim.step(3)
+		self.assertTrue(["ball",0] in result)
+	def test_is_stable(self):
+		self.sim.add_ball("ball", 0.25, 0.5, 0.0, 0.1)
+		self.assertFalse(self.sim.is_stable())
+		self.sim.step(10)
+		self.assertTrue(self.sim.is_stable())
 
 if __name__ == '__main__':
 	unittest.main()
