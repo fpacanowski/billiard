887f3ee6ee8b886576590c452c1ca18f9cd66bb9
Filip Pacanowski <fpacanowski@gmail.com>
2012-01-28 21:56:46 +0100

    ...

--- biliard.py
+++ biliard.py
@@ -2,25 +2,24 @@
 
 import pygame
 from math import sqrt
+import math
 import sys
 import threading
 
-RADIUS=20
-POCKET_RADIUS=30
+RADIUS=10
+POCKET_RADIUS=20
 OFFSET_X=50
-OFFSET_Y=50
+OFFSET_Y=120
 WIDTH=800
 HEIGHT=400
 FPS=60
 FRICTION=0.001/FPS
+VMAX=1.0
 black    = (   0,   0,   0)
 white    = ( 255, 255, 255)
 green    = ( 100, 255, 100)
 red      = ( 255,   0,   0)
  
-
-def distance(pos1, pos2):
-	return (pos1[0]-pos2[0])**2+(pos1[1] - pos2[1])**2
 
 class Ball:
 	def __init__(self,x,y,vx,vy):
@@ -50,8 +49,13 @@ class Simulation:
 		self.processed = set()
 		self.pockets = [[0.0, 0.0], [1.0, 0.0], [2.0, 0.0], \
 		                [0.0, 1.0], [1.0, 1.0], [2.0, 1.0]]
+
+	def distance(self, pos1, pos2):
+		return (pos1[0]-pos2[0])**2+(pos1[1] - pos2[1])**2
+
 	def add_ball(self, label, x, y, vx, vy):
 		self.balls.update({label: Ball(x,y,vx/FPS,vy/FPS)})
+
 	def next_iter(self):
 		self.detect_collisions()
 		for b in self.balls.itervalues():
@@ -59,6 +63,7 @@ class Simulation:
 		res = self.result
 		self.result = []
 		return res
+
 	def count_new_velocity(self, vel1, vel2, pos1, pos2):
 		##print pos1,pos2,vel1,vel2
 		v = [vel2[0]-vel1[0], vel2[1]-vel1[1]]
@@ -77,6 +82,7 @@ class Simulation:
 		##print r 
 		#sys.exit()
 		return r
+
 	def detect_collisions_with_walls(self, label):
 		ball = self.balls[label]
 		r = float(RADIUS)/HEIGHT
@@ -94,23 +100,26 @@ class Simulation:
 		self.processed.add(label)
 		r = float(RADIUS)/HEIGHT
 		for l in self.balls.iterkeys():
-			if l in self.processed:
+			if l == label or (label,l) in self.processed:
 				continue
 			ball2 = self.balls[l]
-			if distance(ball.pos, ball2.pos) < (2*r)**2:
+			if self.distance(ball.pos, ball2.pos) < (2*r)**2:
+				print label, l
 				v1 = (ball.x_velocity, ball.y_velocity)
 				v2 = (ball2.x_velocity, ball2.y_velocity)
-				print label, l
 				(ball.x_velocity,ball.y_velocity,ball2.x_velocity, ball2.y_velocity) = \
 					self.count_new_velocity(v1,v2, ball.pos, ball2.pos)
-			self.processed.add(l)
+			self.processed.add((label,l))
+			self.processed.add((l,label))
+
 	def detect_collisions_with_pockets(self, label):
 		ball = self.balls[label]
-		r = float(RADIUS)/HEIGHT + float(POCKET_RADIUS)/HEIGHT
+		r = float(POCKET_RADIUS)/HEIGHT
 		for pocket_nr in range(6):
 			pocket = self.pockets[pocket_nr]
-			if distance(ball.pos, pocket) < r**2:
+			if self.distance(ball.pos, pocket) < r**2:
 				self.result.append([label,pocket_nr])
+
 	def detect_collisions(self):
 		self.result = []
 		for ball in self.balls.iterkeys():
@@ -136,50 +145,155 @@ class Interface(threading.Thread):
 	def scale_position(self, pos):
 		return (int(0.5*pos[0]*float(WIDTH))+OFFSET_X, int(pos[1]*float(HEIGHT))+OFFSET_Y)
 	def __init__(self):
+		self.angle = 0
+		self.power = 0.5
 		self.balls = {}
-		self.ball_colors = {0: black, 1: red}
+		self.ball_colors = {0: white, 1: red, 2: red, 3: red, 4:red, 5: red,
+				    6:red, 7:red, 8: red, 9: red, 10:red,
+				    11:red, 12: red, 13:red, 14:red, 15:red}
+		self.shots = []
+		self.stable = True
+		self.done=False
+		self.new_game_request = True
 		pygame.init()
 		threading.Thread.__init__(self)
+	def draw_table(self):
+		pockets = [[0, 0], [WIDTH/2, 0], [WIDTH, 0], \
+			   [0, HEIGHT], [WIDTH/2, HEIGHT], [WIDTH, HEIGHT]]
+
+		self.screen.fill(green)
+		pygame.draw.rect(self.screen,(102,51,0),[OFFSET_X,OFFSET_Y,WIDTH,HEIGHT], 20)
+		pygame.draw.rect(self.screen,(0, 102,0),[OFFSET_X,OFFSET_Y,WIDTH,HEIGHT])
+		for x,y in pockets:
+			pygame.draw.circle(self.screen, black, [x+OFFSET_X, y+OFFSET_Y], POCKET_RADIUS)
+
+	def draw_ball(self, label):
+		pos = self.balls[label]
+		col = self.ball_colors[label]
+		pygame.draw.circle(self.screen,black,self.scale_position(pos),RADIUS+1)
+		pygame.draw.circle(self.screen,col,self.scale_position(pos),RADIUS)
+		if label == 0 and self.stable:
+			self.draw_arrow(self.angle, self.scale_position(pos), self.power)
+
+	def draw_arrow(self, angle, pos, length):
+		#pos = [pos[0], pos[1]-int(25*length)]
+#		arrow = pygame.Surface((50,100))
+#		arrow.fill((255,255,255))
+#		pygame.draw.line(arrow, (0,0,0), (25,100), (25,0))
+#		pygame.draw.line(arrow, (0,0,0), (25,0), (20,25))
+#		pygame.draw.line(arrow, (0,0,0), (25,0), (30,25))
+#		arrow.set_colorkey((255,255,255))
+#		arrow = pygame.transform.scale(arrow, (50,int(50*length)))
+#		nar = pygame.transform.rotate(arrow,angle)
+#		nrect = nar.get_rect(center=pos)
+#		pos = [pos[0]-nar.get_height(), pos[1]-nar.get_height()]
+#		self.screen.blit(nar, pos)
+		angle = (angle/180.0)*math.pi
+		#print v*math.sin(angle), v*math.cos(angle)
+		(dx, dy) = (-100*length*math.sin(angle), -100*length*math.cos(angle))
+		pygame.draw.aaline(self.screen, red, pos, [pos[0]+dx,pos[1]+dy])
+	def draw_scores(self):
+		# Create a font
+		font = pygame.font.Font(None, 50)
+
+		# Render the text
+		text = font.render('Player 1: %d'%self.scores[0], True, (255,255, 255), (159, 182, 205))
+
+		# Create a rectangle
+		textRect = text.get_rect()
+
+		# Center the rectangle
+		textRect.centerx = 150
+		textRect.centery = 50
+
+		# Blit the text
+		self.screen.blit(text, textRect)
+	
+
 	def run(self):
-		size = [900,600]
-		screen = pygame.display.set_mode(size)
-	
+		self.screen = pygame.display.set_mode([900,600])
 		pygame.display.set_caption("Bilard")
-	
-		done=False
-	
+
 		clock=pygame.time.Clock()
-		screen.fill(green)
-		pygame.display.update()
-		while done==False:
-			for event in pygame.event.get(): # User did something
-				if event.type == pygame.QUIT: # If user clicked close
-					done=True # Flag that we are done so we exit this loop 
-			screen.fill(green)
-			pygame.draw.rect(screen,black,[OFFSET_X,OFFSET_Y,WIDTH,HEIGHT], 2)
+		while not self.done:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.done=True
+				if event.type == pygame.KEYDOWN and self.stable:
+					if event.key == pygame.K_LEFT:
+						self.angle += 5
+					elif event.key == pygame.K_RIGHT:
+						self.angle -= 5
+					elif event.key == pygame.K_UP:
+						self.power = min(1.0, self.power+0.1)
+					elif event.key == pygame.K_DOWN:
+						self.power = max(0.1, self.power-0.1)
+					elif event.key == pygame.K_SPACE:
+						self.shots.append([self.angle, self.power])
+				if event.type == pygame.KEYDOWN:
+					if event.unicode == 'n':
+						self.new_game_request = True
+					if event.key == pygame.K_ESCAPE:
+						self.done=True
+			self.draw_table()
+			self.draw_scores()
 			for label in self.balls.iterkeys():
-				pos = self.balls[label]
-				col = self.ball_colors[label]
-				pygame.draw.circle(screen,col,self.scale_position(pos),RADIUS)
+				self.draw_ball(label)
 			clock.tick(FPS)
 			pygame.display.update()
 		pygame.quit ()
+		sys.exit()
 
 class Biliard:
+	def new_game(self):
+		self.scores = [0, 0]
+		self.sim.add_ball(0, 0.5, 0.5, 0.0, 0.0)
+		for i in range(6):
+			for j in range(i):
+				print i,j, (i*(i-1))/2+j+1
+				self.sim.add_ball((i*(i-1))/2+j+1, 1.3+i*0.06, 0.5+j*0.06-i*0.03, 0.0, 0.0)
+		
 	def __init__(self):
 		self.gui = Interface()
 		self.gui.start()
 		self.sim = Simulation()
-		self.sim.add_ball(0, 0.2,0.2,-0.3,0.3)
-		self.sim.add_ball(1, 0.7,0.7,-0.6,-0.6)
+		#self.new_game()
+		#self.sim.add_ball(0, 0.5, 0.5, 0.0, 0.0)
+		#self.sim.add_ball(1, 0.6, 0.5, 0.0, 0.0)
+		#self.sim.add_ball(2, 0.7, 0.5, 0.0, 0.0)
+		#self.sim.add_ball(0, 0.2,0.2,0.5,0.2)
+		#self.sim.add_ball(1, 0.7,0.7,-0.2,-0.2)
 		clock = pygame.time.Clock()
-		while True:
+		self.gui.current_player = 0
+		while not self.gui.done:
+			print "*", self.gui.current_player
+			current_player = self.gui.current_player
+			if self.gui.new_game_request:
+				self.gui.new_game_request = False
+				self.new_game()
 			self.gui.balls = {}
+			if not self.gui.stable and self.sim.is_stable():
+				current_player = (current_player+1)%2
+				self.gui.current_player = current_player
+			self.gui.stable = self.sim.is_stable()
 			for label, ball in self.sim.balls.iteritems():
 				self.gui.balls[label] = ball.pos
-			self.sim.next_iter()
+			if len(self.gui.shots) != 0:
+				(angle, power) = self.gui.shots.pop()
+				v = VMAX*power
+				angle = (angle/180.0)*math.pi
+				print v*math.sin(angle), v*math.cos(angle)
+				self.sim.balls[0].x_velocity = -v*math.sin(angle)/FPS
+				self.sim.balls[0].y_velocity = -v*math.cos(angle)/FPS
+			res = self.sim.next_iter()
+			if 0 in [p[0] for p in res]:
+				self.sim.add_ball(0, 0.5, 0.5, 0.0, 0.0)
+				self.scores[current_player] -= 1
+			for ball, pocket in res:
+				if ball != 0:
+					self.scores[current_player] += 1
+			self.gui.scores = self.scores
 			clock.tick(FPS)
-		#self.
 
 if __name__ == '__main__':
 	Biliard()
